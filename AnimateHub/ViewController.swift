8ccd874695dc8157550ad7ecbff4b542e934be3b
wanbao118 <wanbao118@hotmail.com>
2015-12-12 22:51:42 +0800

    Initial Commit

---
+++ AnimateHub/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimateHub
+//
+//  Created by 姜万宝 on 12/12/15.
+//  Copyright © 2015 wanbao118@hotmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
