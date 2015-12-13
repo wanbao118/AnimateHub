1bf9b5309168790ed2c03324cbb18db5f2f44b6c
wanbao118 <wanbao118@hotmail.com>
2015-12-13 14:59:04 +0800

    add input fields for username and password with animation

--- AnimateHub/WBLogonViewController.swift
+++ AnimateHub/WBLogonViewController.swift
@@ -17,6 +17,8 @@ class WBLogonViewController: UIViewController {
     @IBOutlet weak var bubble5: UIImageView!
     @IBOutlet weak var dot: UIImageView!
     @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,11 +27,30 @@ class WBLogonViewController: UIViewController {
         self.bubble3.transform = CGAffineTransformMakeScale(0, 0)
         self.bubble4.transform = CGAffineTransformMakeScale(0, 0)
         self.bubble5.transform = CGAffineTransformMakeScale(0, 0)
-        self.dot.center.x -= self.view.bounds.width/2
+        //self.dot.center.x -= self.view.bounds.width/2 //Not effective
         self.dot.alpha = 0
-        
-        self.logo.center.x -= self.view.bounds.width
+        //self.logo.center.x -= self.view.bounds.width //Not effective
         self.logo.alpha = 0
+        
+        let usernamePaddingView = UIView(frame: CGRectMake(0, 0, 40,40))
+        let passwordPaddingView = UIView(frame: CGRectMake(0, 0, 40,40))
+        self.username.leftView = usernamePaddingView
+        self.username.leftViewMode = .Always
+        
+        self.password.leftView = passwordPaddingView
+        self.password.leftViewMode = .Always
+        
+        let userImageView = UIImageView(image: UIImage(named: "user"))
+        userImageView.frame.origin = CGPoint(x: 11, y: 11)
+        self.username.addSubview(userImageView)
+        //self.username.center.x -= self.view.bounds.width //Not effective
+        self.username.alpha = 0
+        
+        let keyImageView = UIImageView(image: UIImage(named: "key"))
+        keyImageView.frame.origin = CGPoint(x: 11, y: 11)
+        self.password.addSubview(keyImageView)
+        //self.password.center.x -= self.view.bounds.width //Not effective
+        self.password.alpha = 0
         // Do any additional setup after loading the view.
     }
 
@@ -63,6 +84,13 @@ class WBLogonViewController: UIViewController {
         UIView.animateWithDuration(1, delay: 0.5, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.6, options: .AllowAnimatedContent, animations: {
             self.dot.alpha = 1
             self.dot.center.x += self.view.bounds.width/2
+        }, completion: nil)
+        
+        UIView.animateWithDuration(0.4, delay: 0.4, usingSpringWithDamping: 0.4, initialSpringVelocity: 0.4, options: .AllowAnimatedContent, animations: {
+            self.username.alpha = 1
+            self.username.center.x += self.view.bounds.width
+            self.password.alpha = 1
+            self.password.center.x += self.view.bounds.width
         }, completion: nil)
     }
     /*
